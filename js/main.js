76ac4fc091ee8bbac35f90ff88a943294c8bec0e
riyo222za <riyo.riyaad@gmail.com>
2025-02-27 07:30:12 +0200

    Corrected formatting and spelling

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ let burgerMenu = document.querySelector(".burger-menu");
 let isBurgerOpen = false;
 
 burgerBtn.onclick = function () {
-    if (!isBurgerOpen) {
-        burgerMenu.style.display = "block";
-        burgerBtn.style.backgroundPosition = "center left 50px, center";
-        isBurgerOpen = true;
-    }
-    else if (isBurgerOpen) {
-        burgerMenu.style.display = "none";
-        burgerBtn.style.backgroundPosition = "center, center left 50px";
-        isBurgerOpen = false;
-    }
-}
+  if (!isBurgerOpen) {
+    burgerMenu.style.display = "block";
+    burgerBtn.style.backgroundPosition = "center left 50px, center";
+    isBurgerOpen = true;
+  } else if (isBurgerOpen) {
+    burgerMenu.style.display = "none";
+    burgerBtn.style.backgroundPosition = "center, center left 50px";
+    isBurgerOpen = false;
+  }
+};
